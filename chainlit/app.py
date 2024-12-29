0cd5a3d48c2e7e8f73dac6046107af7aea3b7007
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2024-12-29 15:42:31 +0000

    lora and flash attention

--- chainlit/app.py
+++ chainlit/app.py
@@ -6,9 +6,9 @@ from io import BytesIO
 from huggingface_hub import snapshot_download,hf_hub_download
 
 def dlmodels():
-    modelpath=hf_hub_download(repo_id="leejet/FLUX.1-schnell-gguf", filename="flux1-schnell-q2_k.gguf",local_dir="pretrained_models")
+    #modelpath=hf_hub_download(repo_id="leejet/FLUX.1-schnell-gguf", filename="flux1-schnell-q2_k.gguf",local_dir="pretrained_models")
     #modelpath=hf_hub_download(repo_id="leejet/FLUX.1-schnell-gguf", filename="flux1-schnell-q8_0.gguf",local_dir="pretrained_models")
-    #modelpath=hf_hub_download(repo_id="leejet/FLUX.1-dev-gguf", filename="flux1-dev-q3_k.gguf",local_dir="pretrained_models")
+    modelpath=hf_hub_download(repo_id="leejet/FLUX.1-dev-gguf", filename="flux1-dev-q8_0.gguf",local_dir="pretrained_models")
     
     
     #vaepath=hf_hub_download(repo_id="black-forest-labs/FLUX.1-dev", filename="ae.safetensors",local_dir="pretrained_models")
@@ -34,6 +34,8 @@ def initstabledif():
         t5xxl_path=t5path,
         vae_path=vaepath,
         vae_decode_only=True, # Can be True if we dont use img_to_img
+        diffusion_flash_attn=True,
+        lora_model_dir="./pretrained_models/lora/",
         verbose = True
     )
     return stable_diffusion
@@ -45,7 +47,7 @@ def runstabledif(stable_diffusion,prompt):
     seed = random.randint(0, 2 ** 32 - 1)
     output = stable_diffusion.txt_to_img(
         prompt=prompt,
-        sample_steps=4,
+        sample_steps=25,
         cfg_scale=1.0, # a cfg_scale of 1 is recommended for FLUX
         sample_method="euler", # euler is recommended for FLUX
         seed=seed,
