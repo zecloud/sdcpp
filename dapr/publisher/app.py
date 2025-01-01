ac7da7ada632086e32f1b09dd175643ab235285f
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2025-01-01 22:49:38 +0000

    dapr app pub sub

---
+++ dapr/publisher/app.py
@@ -0,0 +1,123 @@
+import chainlit as cl
+import json
+import time
+import os
+import asyncio
+from dapr.aio.clients import DaprClient
+from cloudevents.sdk.event import v1
+#from dapr.ext.grpc import App
+#from dapr.clients.grpc._response import TopicEventResponse
+import uuid
+import re 
+from azure.servicebus.aio import ServiceBusClient
+from azure.identity.aio import DefaultAzureCredential
+
+
+FULLY_QUALIFIED_NAMESPACE = os.environ["SERVICEBUS_FULLY_QUALIFIED_NAMESPACE"]
+QUEUE_NAME = os.environ.get("SERVICEBUS_QUEUE_NAME", "finishedfluxjob")
+DAPR_STORE_NAME=os.getenv("DAPR_STORE_NAME", "fluxstatestore")
+MAX = 64 * 1024 * 1024 # 64MB
+
+@cl.on_chat_start
+async def on_chat_start():
+    
+    print("A new chat session has started!")
+
+    
+@cl.on_message
+async def main(message: cl.Message):
+     
+   
+    # Your custom logic goes here...
+    await cl.Message(
+        content=f"prompt: {message.content}",
+        #elements=image,
+    ).send()
+    stabledif = await diststabledif(message.content)
+    pathimage=json.loads(stabledif)
+    
+    async with DaprClient(max_grpc_message_length=MAX) as client:
+        #Using Dapr SDK to save and get state
+        outputfile=pathimage["message"]
+        resp =await client.get_state(DAPR_STORE_NAME, outputfile)
+        image=resp.data
+        #await clientload_state(DAPR_STORE_NAME, outputfile, img) 
+        await cl.Message(
+            content=f"{outputfile}",
+            elements=image,
+        ).send()
+
+# async def process_message(message) -> TopicEventResponse:
+#     """
+#     Asynchronously processes the message and returns a TopicEventResponse.
+#     """
+
+#     print(f'Processing message: {message.data()} from {message.topic()}...', flush=True)
+#     global in_progress
+#     in_progress = False
+#     return TopicEventResponse('success')
+
+@cl.step(name="image generator...")
+async def diststabledif(prompt:str):
+    async with DaprClient() as d:
+        idsave=string_to_filename(prompt)
+        req_data = {"prompt":prompt,"folder":"chainlit","idsave":idsave }
+#{'id': id, 'message': 'hello world'}
+
+        # Create a typed message with content type and body
+        await d.publish_event(
+            pubsub_name='pubsubcomponent',
+            topic_name='fluxjob',
+            data=json.dumps(req_data),
+            data_content_type='application/json',
+            #publish_metadata={'ttlInSeconds': '100', 'rawPayload': 'false'},
+        )
+        
+        #global in_progress
+        #in_progress = True
+        credential = DefaultAzureCredential()
+        servicebus_client = ServiceBusClient(FULLY_QUALIFIED_NAMESPACE, credential)
+
+        async with servicebus_client:
+            receiver = servicebus_client.get_queue_receiver(queue_name=QUEUE_NAME)
+            async with receiver:
+                received_msgs = await receiver.receive_messages(max_message_count=1, max_wait_time=600)
+                msg = received_msgs[0]  
+                #for msg in received_msgs:
+                print(str(msg))
+                # await cl.Message(
+                #         content=f"{msg}",
+                #         #elements=image,
+                #     ).send()
+                await receiver.complete_message(msg)
+                return str(msg)
+        # close_fn = await d.subscribe_with_handler(
+        #     pubsub_name='pubsubcomponent', topic='finishedfluxjob/{idsave}', handler_fn=process_message,
+        #     dead_letter_topic='finishedfluxjob/{idsave}_DEAD'
+        # )
+        
+        #while in_progress:
+        #     await asyncio.sleep(1)
+        #global img
+        
+
+        # print("Closing subscription...")
+        # await close_fn()
+
+    #return "message envoyé"
+
+def string_to_filename(input_string):
+    # Remplacer les caractères non valides par des underscores
+    filename = re.sub(r'[^\w\-_\. ]', '_', input_string)
+    # Optionnel: Tronquer le nom de fichier s'il est trop long
+    max_length = 100
+    return filename[:max_length]
+
+
+
+
+
+
+
+
+    
---
+++ dapr/subscriber/app.py
@@ -0,0 +1,132 @@
+import json
+import os
+from time import sleep
+import random
+from io import BytesIO
+from cachetools import cached
+from stable_diffusion_cpp import StableDiffusion
+from dapr.clients import DaprClient
+from dapr.ext.grpc import App, BindingRequest
+from huggingface_hub import snapshot_download,hf_hub_download
+import logging
+from cloudevents.sdk.event import v1
+from dapr.clients.grpc._response import TopicEventResponse
+
+logging.basicConfig(level = logging.INFO)
+app = App()
+#should_retry = True  
+#DAPR_INPUT_STORE_NAME="hotshotoutput"
+DAPR_STORE_NAME=os.getenv("DAPR_STORE_NAME", "statestore")
+#tmpdir="/home/outputs/tmp/"
+MAX = 64 * 1024 * 1024 # 64MB
+SUPERMAX = 512 * 1024 * 1024 # 512MB
+
+def dlmodels():
+    #modelpath=hf_hub_download(repo_id="leejet/FLUX.1-schnell-gguf", filename="flux1-schnell-q2_k.gguf",local_dir="pretrained_models")
+    #modelpath=hf_hub_download(repo_id="leejet/FLUX.1-schnell-gguf", filename="flux1-schnell-q8_0.gguf",local_dir="pretrained_models")
+    #modelpath=hf_hub_download(repo_id="leejet/FLUX.1-dev-gguf", filename="flux1-dev-q3_k.gguf",local_dir="pretrained_models")
+    modelpath=hf_hub_download(repo_id="leejet/FLUX.1-dev-gguf", filename="flux1-dev-q8_0.gguf",local_dir="pretrained_models")
+    
+    
+    #vaepath=hf_hub_download(repo_id="black-forest-labs/FLUX.1-dev", filename="ae.safetensors",local_dir="pretrained_models")
+    vaepath=hf_hub_download(repo_id="Green-Sky/flux.1-schnell-GGUF", filename="ae-f16.gguf",local_dir="pretrained_models")
+    
+    #clippath=hf_hub_download(repo_id="comfyanonymous/flux_text_encoders", filename="clip_l.safetensors",local_dir="pretrained_models")
+    #t5path=hf_hub_download(repo_id="comfyanonymous/flux_text_encoders", filename="t5xxl_fp16.safetensors",local_dir="pretrained_models")
+    clippath=hf_hub_download(repo_id="Green-Sky/flux.1-schnell-GGUF", filename="clip_l-q8_0.gguf",local_dir="pretrained_models")
+    #clippath=hf_hub_download(repo_id="zer0int/CLIP-GmP-ViT-L-14", filename="ViT-L-14-TEXT-detail-improved-hiT-GmP-TE-only-HF.safetensors",local_dir="pretrained_models")
+
+   
+    t5path=hf_hub_download(repo_id="Green-Sky/flux.1-schnell-GGUF", filename="t5xxl_q8_0.gguf",local_dir="pretrained_models")
+    return modelpath,vaepath,clippath,t5path
+
+@cached(cache={})
+def initstabledif():    
+    modelpath,vaepath,clippath,t5path=dlmodels()
+    stable_diffusion = StableDiffusion(
+        diffusion_model_path=modelpath, # In place of model_path
+        clip_l_path= clippath,
+        t5xxl_path=t5path,
+        vae_path=vaepath,
+        vae_decode_only=True, # Can be True if we dont use img_to_img
+        diffusion_flash_attn=True,
+        lora_model_dir="./pretrained_models/lora/",
+        verbose = True
+    )
+    return stable_diffusion
+
+# def message_handler(message_content):
+#     """
+#     Fonction personnalisée pour gérer les messages.
+    
+#     :param message_content: Contenu du message provenant de la file d'attente.
+#     """
+#     imgrequested=json.loads(message_content)
+#     genimg(imgrequested)
+
+def runstabledif(stable_diffusion,prompt):
+    seed = random.randint(0, 2 ** 32 - 1)
+    output = stable_diffusion.txt_to_img(
+        prompt=prompt,
+        sample_steps=25,
+        cfg_scale=1.0, # a cfg_scale of 1 is recommended for FLUX
+        sample_method="euler", # euler is recommended for FLUX
+        seed=seed,
+    )
+    bIO = BytesIO()
+    output[0].save(bIO, format="PNG")
+    data_bytes=bIO.getvalue()
+    return data_bytes
+
+def genimg(params):
+    stable_diffusion=initstabledif()
+    prompt=params["prompt"]
+    folder=params["folder"]
+    idsave=params["idsave"]
+    data_bytes=runstabledif(stable_diffusion,prompt)
+    publish_and_save(data_bytes,folder,idsave)
+
+def publish_and_save(img,folder,idsave):
+  #stringimg = base64.b64encode(image).decode('utf-8')
+  with DaprClient(max_grpc_message_length=MAX) as client:
+        #Using Dapr SDK to save and get state
+        outputfile=folder+"/"+idsave+".png"
+        client.save_state(DAPR_STORE_NAME, outputfile, img) 
+        req_data = {'message':  outputfile} 
+        # Create a typed message with content type and body
+        resp = client.publish_event(
+            pubsub_name='pubsubcomponent',
+            topic_name=f'finishedfluxjob',#/{idsave}
+            data=json.dumps(req_data),
+            data_content_type='application/json',
+            publish_metadata={'rawPayload': 'true'},
+        )
+        logging.info('Published data: message sent')
+        print('sent message and saved')
+
+@app.subscribe(pubsub_name='pubsubcomponent', topic='fluxjob')
+def mytopic(event: v1.Event) -> TopicEventResponse:
+    #global should_retry
+    data = json.loads(event.Data())
+    print(f'Received: idsave={data["idsave"]}, prompt="{data["prompt"]}"' 
+          ' content_type="{event.content_type}"',flush=True)
+    try:
+        genimg(data)
+    except Exception as e:
+        print(f"Error processing message: {e}")
+        return TopicEventResponse('drop')
+    # if should_retry:
+    #     should_retry = False  # we only retry once in this example
+    #     sleep(0.5)  # add some delay to help with ordering of expected logs
+    #     return TopicEventResponse('retry')
+    return TopicEventResponse('success')
+
+# @app.binding('holavatarsinput')
+# def incoming(request: BindingRequest):
+#     incomingtext = request.text()
+#     print(">>>>>>>Message Received: "+ incomingtext,flush=True)
+
+if __name__ == '__main__':
+    initstabledif()
+    app.register_health_check(lambda: print('Healthy'))
+    app.run(50051)
